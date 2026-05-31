d964dd2957524475fc20a3769c1dea506c4a697f
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-05-31 20:35:54 +0300

    Add first solution for Week 1

---
+++ Week-01/10_RemoveAllAdjacentDuplicatesInString_Ashraf.cpp
@@ -0,0 +1,18 @@
+// Author: Ashraf
+// Link: https://leetcode.com/problems/remove-all-adjacent-duplicates-in-string/
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string temp = "";
+        for (int i = 0; i < s.length(); i++) {
+            if (temp.empty() || s[i] != temp.back()) {
+                temp.push_back(s[i]);
+            } else {
+                temp.pop_back();
+            }
+        }
+        return temp;
+    }
+};
