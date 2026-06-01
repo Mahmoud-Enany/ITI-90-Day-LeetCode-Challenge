00a419771c1041d4fb2491859d47af515d1d6f49
Ashraf3x <ahmedashrafmohamed112@gmail.com>
2026-06-01 20:29:13 +0300

    [Week-01] Backspace String Compare - Ashraf

--- Week-01/02-Backspace String Compare/Ashraf.cpp
+++ Week-01/02-Backspace String Compare/Ashraf.cpp
@@ -5,21 +5,25 @@
 class Solution {
 public:
     bool backspaceCompare(string s, string t) {
-        string temp1 = "", temp2 = "";
-        for (auto it : s) {
-            if (it == '#' && !temp1.empty())
-                temp1.pop_back();
-            else
+     string temp1 = "", temp2 = "";   
+     for (auto it : s) {
+            if (it == '#') {
+                if (!temp1.empty()) {
+                    temp1.pop_back();
+                }
+            } else {
                 temp1 += it;
+            }
         }
         for (auto it : t) {
-            if (it == '#' && !temp2.empty())
-                temp2.pop_back();
-            else
+            if (it == '#') {
+                if (!temp2.empty()) {
+                    temp2.pop_back();
+                }
+            } else {
                 temp2 += it;
+            }
         }
-        while(*temp1.begin()=='#')temp1.erase(temp1.begin());
-        while(*temp2.begin()=='#')temp2.erase(temp2.begin());
         return temp1 == temp2;
     }
 };
